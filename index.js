de5e878f6a5b2663c1067faa5dccdc915baa41b3
Ramoulow <105707563+Ramoulow@users.noreply.github.com>
2022-09-27 11:38:14 +0200

    fin de l'exemple de routage pour création de site

--- index.js
+++ index.js
@@ -1,10 +1,48 @@
 "use strict";
 
-const express = require('express')
-const app = express()
+//  apport du module Express
+const express = require('express');
 
-app.get('/', function (req, res) {
-  res.send('Hello world');
-})
+// Instance du module Express qu'on appelle "app"
+const app = express();
 
-app.listen(3000)
+// Définition du port d'écoute
+const port = 4242;
+
+
+
+// Routage
+// =======
+
+// Route 1
+// -------
+
+// Définition de la route pour la page d'accueil
+const home_route = "/";
+
+// Définition du controlleur de la page d'accueil
+const home_controller = function (req, res) {
+  res.send("Hello world");
+}
+
+// Ajout de la route dans le registre de l'application de Node
+app.get(home_route, home_controller);
+
+// Route 2
+// -------
+
+const page2_route = "/page_2";
+const page2_controller = function (req, res) {
+  res.send("Ceci est la page 2.");
+};
+app.get(page2_route, page2_controller);
+
+// Route 3
+// -------
+app.get("/page_3", (req, res) => {
+  res.send("Ceci est la page 3.");
+});
+
+
+
+app.listen(port);
